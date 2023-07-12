318614935a8baa6ab60f005fcbb245f359328b00
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-07-12 18:02:00 +0300

    first commit-Displaying Question

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+
+  //"loading", "ready", "error", "active", "finished"
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
+  const [{ status, questions, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // console.log(status);
+  // console.log(questions);
+
+  const numQuestions = questions.length;
+  //console.log(numQuestions);
+
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const resp = await fetch("http://localhost:8000/questions");
+        //console.log(resp);
+        if (!resp.ok) throw new Error("Something went wrong with fetching");
+        const data = await resp.json();
+        //console.log(data);
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (e) {
+        dispatch({ type: "dataFailed" });
+        console.error(e.message);
+      }
+    }
+
+    fetchQuestions();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "error" && <Error />}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
     </div>
   );
 }
